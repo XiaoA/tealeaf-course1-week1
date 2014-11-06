a48fcf601fbb6b6a76d17e9a9bf4d30d683150ba
Andrew Buckingham <abuckingham99@gmail.com>
2014-11-06 07:38:55 -0600

    Fix indentation issues

--- pass_by_reference_vs_pass_by_value.rb
+++ pass_by_reference_vs_pass_by_value.rb
@@ -1,28 +1,28 @@
 # def some_method(obj)
-  #   obj.uniq 
-  # end
+#   obj.uniq 
+# end
   
-  # outer_obj = [1, 2, 2, 3, 3]
-  # some_method(outer_obj)  
+# outer_obj = [1, 2, 2, 3, 3]
+# some_method(outer_obj)  
   
-  # puts outer_obj
+# puts outer_obj
   
-  # Calling a method that does not mutate the caller
-    def safe_method(obj)
-      obj.bsearch {|x| x > 4 } 
-    end
+# Calling a method that does not mutate the caller
+def safe_method(obj)
+  obj.bsearch {|x| x > 4 } 
+end
     
-  arr_one = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  puts "Before the 'bsearch' method is called, the value of 'arr_one' is #{arr_one}."
-  puts "\n"
-  puts "Let's call the method:"
-  p safe_method(arr_one)
-  puts "\n"  
-  puts"After the 'bsearch' method is called, the value of 'arr_one' is #{arr_one}."
-  puts "The 'bsearch' method is non-destructive."
-  puts "\n"
-  puts "------------"
-  puts "\n"
+arr_one = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+puts "Before the 'bsearch' method is called, the value of 'arr_one' is #{arr_one}."
+puts "\n"
+puts "Let's call the method:"
+p safe_method(arr_one)
+puts "\n"  
+puts"After the 'bsearch' method is called, the value of 'arr_one' is #{arr_one}."
+puts "The 'bsearch' method is non-destructive."
+puts "\n"
+puts "------------"
+puts "\n"
 # calling a method that mutates the caller within the method
 
 def unsafe_method(obj)
