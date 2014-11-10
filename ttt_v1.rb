eb6b40ad03df9fe86fbe2094264deb5138077e8e
Andrew Buckingham <abuckingham99@gmail.com>
2014-11-09 22:07:32 -0600

    Remove hash from ttt_v1.rb

--- ttt_v1.rb
+++ ttt_v1.rb
@@ -1,13 +1,42 @@
 # A simple Tic, Tac, Toe game, built with Ruby
+
+#############################
+# Application Logic
+#############################
+
+# Game Overview
+# - Needs a board, with 9 places
+# - Needs to have a way for the player and the computer to take turns playing
+# - Needs to take input and represent that on the board with an 'X' or an 'O'
+# - Needs to declare a winner
+
+# Game Steps
+# 1. Draw a board
+# 2. Assign numbers to each position/square
+# 3. Get user input (which square?)
+# 4. Computer chooses a square
+# 5. Iterate until a winner is chosen
+
 require 'pry'
 
-board_position = {1 => " ", 2 => " ", 3 => " ", 4 => " ", 5 => " ", 6 => " ", 7 => " ", 8 => " ", 9 => " "}     
+def draw_board
+  puts "      |      |      "
+  puts "      |      |      "
+  puts "------+------+-------"
+  puts "      |      |      "
+  puts "      |      |      "
+  puts "------+------+-------"
+  puts "      |      |      "
+  puts "      |      |      "
+end
 
-puts "      |      |      "
-puts "#{board_position[1]}     |#{board_position[2]}     |#{board_position[3]}     "
-puts "------+------+-------"
-puts "      |      |      "
-puts "#{board_position[4]}     |#{board_position[5]}     |#{board_position[6]}     "
-puts "------+------+-------"
-puts "      |      |      "
-puts "#{board_position[7]}     |#{board_position[8]}     |#{board_position[9]}     "
+draw_board
+
+
+puts "Please choose a square (1 ~ 9)
+player_choice = gets.chomp.to_i
+
+    
+
+
+
