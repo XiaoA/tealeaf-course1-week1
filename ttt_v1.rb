35c27cd0a83978f56b24be465774ce43ddef69c6
Andrew Buckingham <abuckingham99@gmail.com>
2014-11-10 18:27:31 -0600

    Add methods to  'ttt_v1.rb

--- ttt_v1.rb
+++ ttt_v1.rb
@@ -1,12 +1,12 @@
 # A simple Tic, Tac, Toe game, built with Ruby
 require 'pry'
 
-# 1. we create a hash as the data structure to represent the board
+
 board = {1 => " ", 2 => " ", 3 => " ", 4 => " ", 5 => " ", 6 => " ", 7 => " ", 8 => " ", 9 => " "}     
 
-# 2. we draw the board on the screen using puts, and also, we wrap them into a method, so that we could call it later (every time, either player or computer places a piece, we will need to call this method, to re-draw the board)
 def draw_board(board)
   system 'clear'
+  
   puts "      |      |      "
   puts "#{board[1]}     |#{board[2]}     |#{board[3]}     "
   puts "------+------+-------"
@@ -17,20 +17,35 @@ def draw_board(board)
   puts "#{board[7]}     |#{board[8]}     |#{board[9]}     "
 end
 
-
-# 3. let's create a method for player to place a piece, placing a piece is essentially settting the board hash value
-
+def empty_square(board)
+  board.select {|k, v| v ==  " " }
+end
 
 def player_places_piece(board) # pass in board hash as the parameter, so that we can set it within the method
+  puts "Pick a square (1 ~ 9)."
   position = gets.to_i # get the user input to decide where player wants to place his or her piece
   board[position] = 'X' # set the hash value
 end
 
 
-# 4. same thing with computer placing a piece, instead of taking user input, the position number should be generated randomly
+
 def computer_places_piece(board)
+  position = empty_square(board).sample
 end
 
+# def check_for_winner(board)
+#   case
+#   when player_places_piece == 
+# end
+
+begin
+  draw_board(board)
+  player_places_piece(board)
+  empty_square(board)
+  computer_places_piece(board)
+end
+  
+  
 # 5. do...while (it should be begin...end until here) loop, to keep the game running until there's a winner or board gets full
 draw_board(board)
 while true
